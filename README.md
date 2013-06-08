212480e8f2322883f72e6f3b0f3c71e49bc29d72
Susam Pal <susam@susam.in>
2013-06-09 02:13:47 +0530

    Add timebox 0.1
    This is the first version of the timeboxing script released under the
    terms of the Simplified BSD License (BSD 2-Clause License). This initial
    commit contains the script, a readme, and the license.

---
+++ README.md
@@ -0,0 +1,110 @@
+Introduction
+------------
+Timeboxing is a time management technique that is believed to boost
+productivity by limiting the time during which a task is supposed to be
+completed. A time box is a fixed period of time alloted for a task or
+activity. The period of time to spend on the task is decided first. One
+time box may last anywhere between 15 minutes to 45 minutes. The
+duration of a time box may depend on the task or activity.
+Alternatively, the task may be scoped in a manner that it can be
+completed in a time box. Then a timer is started with the decided time
+interval. Once the timer notifies that the time interval has expired,
+any activity or work on the task is stopped, and a short break is taken
+before beginning another time box.
+
+This project offers a script called timebox.cmd that runs on Microsoft
+Windows systems. It can be used to run a time box for a specified
+duration.
+
+
+Installation
+------------
+Since this is just a one file script, installing it simply requires
+downloading the timebox.cmd file and copying it to some directory
+present in the Windows PATH variable. To download this script, visit
+<https://github.com/susam/timeboxing> and click the 'ZIP' button. Then
+unzip the downloaded file and copy the timebox.cmd file from it into a
+directory specified in the Windows PATH variable.
+
+There are various ways to check the Windows PATH variable.
+
+  1. Open 'Command Prompt' and run the command: echo %PATH%.
+  2. Hold down the 'Windows' key and press the 'Break' key to launch the
+     'System Properties'. It can also be launched by right clicking
+     'My Computer' and selecting 'Properties'. If you are on
+     Windows Vista, Windows 7 or a later version of Windows, click
+     'Advanced system settings'. Click 'Advanced' tab. Click
+     'Environment variables' button. Look for the 'PATH' variable in
+     'System variables' section.
+
+Once timebox.cmd is placed in a directory specified in the Windows PATH
+variable, it can be launched from command prompt by simply executing the
+command:
+
+    timebox
+
+The above command runs a 30 minute time box.
+
+
+Getting started
+---------------
+To run a 30 minute time box, run the script without any arguments.
+
+    timebox
+
+The script accepts one integer argument that specifies the duration of
+the time box in minutes. For example, the following command also runs a
+30 minute time box.
+
+    timebox 30
+
+The following command runs a 15 minute time box.
+
+    timebox 15
+
+To learn more about the usage of the script, run the following command.
+
+    timebox --help
+
+
+Usage
+-----
+    Usage: timebox [OPTION] [DURATION]
+
+    The timeboxing script runs for the number of minutes specified
+    as the duration argument. If no duration argument is specified,
+    it runs for 30 minutes. If the duration argument is echo not
+    a multiple of 5, it is rounded down to a multiple of 5.
+
+    Options:
+      -w, --where     display the path where this script is present
+      -h, --help, /?  display this help and exit
+      -v, --version   display version information and exit
+
+    Examples:
+      timebox         run a 30 minute time box
+      timebox 15      run a 15 minute time box
+      timebox 12      run a 10 minute time box
+
+    Report bugs to <susam@susam.in>.
+
+
+Version
+-------
+    timebox 0.1
+    Copyright (c) 2013 Susam Pal
+
+
+License
+-------
+This is free software. You are permitted to redistribute and use it in
+source and binary forms, with or without modification, under the terms
+of the Simplified BSD License. See the LICENSE.txt file for the complete
+license.
+
+This software is provided WITHOUT ANY WARRANTY; without even the implied
+warranties of MERCHANTABILITY and FITNESS FOR A PARTICULAR PURPOSE. See
+the LICENSE.txt file for the complete disclaimer.
+
+If you do not have a copy of the LICENSE.txt file, please visit
+<http://susam.in/licenses/bsd/> to obtain a copy of the license.
---
+++ timebox.cmd
@@ -0,0 +1,287 @@
+@echo off
+
+goto :start
+rem Timeboxing script
+
+rem Copyright (c) 2013 Susam Pal
+rem All rights reserved.
+rem
+rem Redistribution and use in source and binary forms, with or without
+rem modification, are permitted provided that the following conditions
+rem are met:
+rem     1. Redistributions of source code must retain the above copyright
+rem        notice, this list of conditions and the following disclaimer.
+rem     2. Redistributions in binary form must reproduce the above
+rem        copyright notice, this list of conditions and the following
+rem        disclaimer in the documentation and/or other materials provided
+rem        with the distribution.
+rem
+rem THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+rem "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+rem LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+rem A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+rem HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+rem SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+rem LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+rem DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+rem THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+rem (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+rem OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+:start
+    setlocal
+
+    rem Script data
+    set VERSION=0.1
+    set COPYRIGHT=Copyright (c) 2013 Susam Pal
+    set LICENSE_URL=http://susam.in/licenses/bsd/
+
+    rem Script settings
+    set LOG=Yes
+    set TIME_LOG=%~dp0\timeboxes.txt
+    set DEFAULT_DURATION=30
+
+    rem Determine how the script is executed and set global variables
+    rem accordingly
+    if /i "%COMSPEC%" == "%CMDCMDLINE%" (
+        set MODE=Console
+        set NAME=%0
+    ) else (
+        set MODE=Explorer
+        set NAME=%~nx0
+    )
+
+    rem Handle command line arguments
+    if %~1. == . (
+        call :timer %DEFAULT_DURATION%
+    ) else if %~1. == -w. (
+        call :where_am_i
+    ) else if %~1. == --where. (
+        call :where_am_i
+    ) else if %~1. == -h. (
+        call :help
+    ) else if %~1. == --help. (
+        call :help
+    ) else if %~1. == /?. (
+        call :help
+    ) else if %~1. == -v. (
+        call :version
+    ) else if %~1. == --version. (
+        call :version
+    ) else if %~1. == --qtpi. (
+        call :qtpi
+    ) else (
+        call :timer %~1
+    )
+
+    endlocal
+    goto :eof
+
+:timer
+    setlocal enabledelayedexpansion
+    set /a duration = %~1
+
+    rem Validate duration
+    if %duration% EQU 0 (
+        call :bad_duration_error
+        goto :eof
+    ) else if %duration% LSS 5 (
+        call :less_duration_error
+        goto :eof
+    ) else if %duration% GTR 90 (
+        call :more_duration_error
+        goto :eof
+    )
+
+    rem Round down duration to a multiple of five
+    set /a duration = (%duration% / 5) * 5
+
+    rem Beep and start timer
+    call :beep 2
+    for /l %%i in (%duration%, -5, 5) do (
+        rem Display time left
+        set time_left=0%%i
+        echo !time:~0,-3! - !time_left:~-2!
+
+        call :pre_sleep %duration% %%i
+        call :sleep 300
+    )
+
+    rem End timer
+    echo %time:~0,-3% - 00
+    if %LOG%. == Yes. (
+        set duration=0%duration%
+        echo %date% %time:~0,-3% - !duration:~-2!>>%TIME_LOG%
+    )
+
+    rem Beep and display smileys
+    call :beep 4
+    echo     
+    msg %username% /w /time:10 ":-)    :-)    :-)    :-)    :-)"
+
+    endlocal
+    goto :eof
+
+:pre_sleep
+    setlocal
+
+    rem Beep if fifteen minutes remain in a time box longer than
+    rem fifteen minutes
+    set duration=%~1
+    set time_left=%~2
+    if not %duration% == 15 (
+        if %time_left% == 15 (
+            call :beep 1
+        )
+    )
+
+    endlocal
+    goto :eof
+
+:sleep
+    setlocal
+    set /a seconds = %~1 + 1
+    ping -n %seconds% 127.0.0.1 > nul
+    endlocal
+    goto :eof
+
+:beep
+    setlocal
+    if %~1. == . (
+        set /a count = 1
+    ) else (
+        set /a count = %~1
+    )
+    for /l %%i in (1, 1, %count%) do (
+        set /p a="" < nul
+    )
+    endlocal
+    goto :eof
+
+:string_length
+    setlocal enabledelayedexpansion
+    set str=%~1.
+    set len=0
+    for %%i in (256 128 64 32 16 8 4 2 1) do (
+        if not "!str:~%%i,1!" == "" (
+            set /a len += %%i
+            set str=!str:~%%i!
+        )
+    )
+    (
+        endlocal
+        set %~2=%len%
+        goto :eof
+    )
+
+:echo_newlines
+    for /l %%i in (1, 1, %~1) do (
+        echo.
+    )
+    goto :eof
+
+:echo_center
+    setlocal enabledelayedexpansion
+    set msg=%~1
+    call :string_length "%msg%" len
+    set /a horizontal_gap = (80 - %len%) / 2
+    for /l %%i in (1, 1, %horizontal_gap%) do (
+        set msg= !msg!
+    )
+    echo %msg%
+    endlocal
+    goto :eof
+
+:where_am_i
+    echo %~f0
+    if %MODE% == Explorer (
+        pause > NUL
+    )
+    goto :eof
+
+:help
+    setlocal
+    set DD=%DEFAULT_DURATION%
+    echo Usage: %NAME% [OPTION] [DURATION]
+    echo.
+    echo The timeboxing script runs for the number of minutes specified
+    echo as the duration argument. If no duration argument is specified,
+    echo it runs for %DD% minutes. If the duration argument is echo not
+    echo a multiple of 5, it is rounded down to a multiple of 5.
+    echo.
+    echo Options:
+    echo   -w, --where     display the path where this script is present
+    echo   -h, --help, /?  display this help and exit
+    echo   -v, --version   display version information and exit
+    echo.
+    echo Examples:
+    echo   %NAME%         run a %DD% minute time box
+    echo   %NAME% 15      run a 15 minute time box
+    echo   %NAME% 12      run a 10 minute time box
+    echo.
+    echo Report bugs to ^<susam@susam.in^>.
+    if %MODE% == Explorer (
+        pause > NUL
+    )
+    endlocal
+    goto :eof
+
+:version
+    echo %NAME% %VERSION%
+    echo %COPYRIGHT%
+    echo.
+    echo This is free software. You are permitted to redistribute and use it in
+    echo source and binary forms, with or without modification, under the terms
+    echo of the Simplified BSD License. See ^<%LICENSE_URL%^> for
+    echo the complete license.
+    echo.
+    echo Written by Susam Pal.
+    if %MODE% == Explorer (
+        pause > NUL
+    )
+    goto :eof
+
+:qtpi
+    setlocal enabledelayedexpansion
+    set dedication=For Sunaina, for all time
+    if %mode% == Console (
+        echo.
+        echo     %dedication%
+    ) else (
+        call :echo_newlines 11
+        call :echo_center "%dedication%"
+        call :echo_newlines 12
+        pause > NUL
+    )
+    endlocal
+    goto :eof
+
+:bad_duration_error
+    setlocal
+    set error_msg=ERROR: Duration must be a positive integer
+    echo %error_msg%
+    if %MODE% == Explorer (
+        pause > NUL
+    )
+    endlocal
+    goto :eof
+
+:less_duration_error
+    setlocal
+    set error_msg=ERROR: Duration must be at least 5 minutes
+    echo %error_msg%
+    if %MODE% == Explorer (
+        pause > NUL
+    )
+    endlocal
+    goto :eof
+
+:more_duration_error
+    setlocal
+    set error_msg=ERROR: Duration must not exceed 90 minutes
+    echo %error_msg%
+    if %MODE% == Explorer (
+        pause > NUL
+    )
+    endlocal
+    goto :eof
